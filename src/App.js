d19e331367ec1055e9d6f9fbae916ed03bd0af1a
hiagrawal <agrawal.hina13@gmail.com>
2021-03-26 12:31:06 +0530

    update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
 import {HomePage} from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <h1>Hats Page</h1>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path='/' component= {HomePage}></Route>
+        <Route exact path='/hats' component= {HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`}>
+// withRouter is a Higher Order Component. Higer Order Component is a function that takes component 
+// as an argument and returns a new modified component. 
+
+
+const MenuItem = ({title, imageUrl, size, history, match, linkUrl}) => (
+    <div className={`menu-item ${size}`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`
         }}/>
@@ -15,4 +20,9 @@ const MenuItem = ({title, imageUrl, size}) => (
 
 )
 
-export default MenuItem;
+export default withRouter(MenuItem); 
+//by wrapping like this, it wil have access to all history, location and match props
+//otherwise only homepage component (or any component in which route is being called - component name given in Route in App.js for specific route) 
+//has ideally access to these router props (history, location and match) and to access the same in other 
+//components , will have to pass the same as props in child components 
+//which is called props drilling or props tunneling. To avoid this, HOC (Higher Order Components) are being used
