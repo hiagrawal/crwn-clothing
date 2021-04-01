ca362b04e0639da2a56a758adaa6375984457ebe
hiagrawal <agrawal.hina13@gmail.com>
2021-04-01 16:47:40 +0530

    integrated firebase storage and storing user data in firebase and in our app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,29 @@ const config = {
     measurementId: "G-ZGR9053MCR"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    //console.log(firestore.doc('/users/Pr6UkKUkfT0Xt0NB3cwp/cartItems/OPyWU7CZQS8sBykdATLK'));
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    console.log('userRef in firebase utility:', userRef);
+    const snapShot = await userRef.get();
+    console.log('snapShot in firebase utility:',snapShot);
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try{
+            await userRef.set({displayName,email,createdAt,...additionalData})
+        }
+        catch(error){
+            console.log("error creating user", error.message);
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
@@ -22,3 +45,7 @@ var provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => firebase.auth().signInWithPopup(provider);
 
 export default firebase;
+
+// firestore.collection('users').doc('Pr6UkKUkfT0Xt0NB3cwp').collection('cartItems').doc('OPyWU7CZQS8sBykdATLK');
+// firestore.doc('/users/Pr6UkKUkfT0Xt0NB3cwp/cartItems/OPyWU7CZQS8sBykdATLK');
+// firestore.collection('/users/Pr6UkKUkfT0Xt0NB3cwp/cartItems');
