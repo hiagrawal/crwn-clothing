4b33beb57e8cb9586b0f90ca3cd8684dddd9c649
hiagrawal <agrawal.hina13@gmail.com>
2021-04-08 13:54:57 +0530

    creating item count in cart icon using selector concept in redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,10 +5,10 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 
-const CartIcon =({toggleCartHidden}) =>(
+const CartIcon =({toggleCartHidden,itemCount}) =>(
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -16,4 +16,11 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    itemCount: cartItems.reduce((totalCount, item) => totalCount + item.quantity,0)
+})
+//this itemCount concept is called 'selector in Redux' as we are taking the state value
+//and not directly using the complete state value that is cartItems in this case and reducing it to take a slice of it
+// which is itemCount
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
