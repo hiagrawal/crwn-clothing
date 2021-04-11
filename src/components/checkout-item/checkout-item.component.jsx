89bad0acd6c576637bda6243f29c78660e6035bf
hiagrawal <agrawal.hina13@gmail.com>
2021-04-11 14:20:01 +0530

    created clear item action and it's reducer and removed item on click of cross on checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,12 @@
 import React from 'react';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({cartItem:{imageUrl, name, quantity, price}}) => (
+import {connect} from 'react-redux';
+import {clearItemFromCart} from '../../redux/cart/cart.actions';
+
+const CheckoutItem = ({cartItem ,clearItem}) => {
+    const {imageUrl, name, quantity, price} = cartItem;
+    return(
     <div className="checkout-item">
         <div className="image-container">
             <img src={imageUrl} alt="item" className="img"/>
@@ -10,9 +15,13 @@ const CheckoutItem = ({cartItem:{imageUrl, name, quantity, price}}) => (
         <span className="name">{name}</span>
         <span className="quantity">{quantity}</span>
         <span className="price">{price}</span>
-        <div className="remove-button">&#10005;</div>
+        <div className="remove-button" onClick={()=>clearItem(cartItem)}>&#10005;</div>
 
     </div>
-) 
+)}
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
