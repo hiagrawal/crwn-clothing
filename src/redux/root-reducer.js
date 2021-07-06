bd5d51b928638b57a030c4f1afbfc6b0a344d21b
hiagrawal <agrawal.hina13@gmail.com>
2021-07-06 14:41:19 +0530

    storing the state of cart on refresh in local storage using redux-persist library

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,7 +3,20 @@ import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
+//here will create an config object which will define: 1. Where want to store, what type of storage : local or session and what data want to store
+//whitelist will have an array of reducers that we want to store
+
+const rootReducer = combineReducers({
     user:userReducer,
     cart:cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,15 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 
+import { persistStore } from 'redux-persist';
+//allows the browser to cache the store 
+
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export {store, persistor};
