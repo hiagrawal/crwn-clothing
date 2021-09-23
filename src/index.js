dca1eeb9e89f6dbe6379d91abd3bbad1d1fdf965
hiagrawal <agrawal.hina13@gmail.com>
2021-09-23 12:08:13 +0530

    making application a PWA.. registered service worker file, added icons for 192 512 in manifest.json file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux';
 import {store,persistor} from './redux/store';
 import {PersistGate} from 'redux-persist/integration/react'; 
 
-
+import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
 <Provider store={store}>
@@ -18,5 +18,7 @@ ReactDOM.render(
     </BrowserRouter>
 </Provider>,
 document.getElementById('root'));
+
+serviceWorker.register();
 
 
