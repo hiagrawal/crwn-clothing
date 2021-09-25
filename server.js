4521090e6b94b8bde04e8a4a7600e96b4088e541
hiagrawal <agrawal.hina13@gmail.com>
2021-09-25 13:45:31 +0530

    Making application PWA complaint by serving reuests to https, providing service worker.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 //console.log('stripe secret key' + process.env.STRIPE_SECRET_KEY);
@@ -13,7 +15,14 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  //compression algorithm does is gzip the files so they're smaller
+
+  app.use(enforce.HTTPS({ trustProtoHeader : true}));
+  //This is to make the application PWA complaint that is to enforce all requests through HTTPS only
+  
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -25,6 +34,11 @@ app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
 });
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+//This is to make PWA complaint to get service worker file
 
 app.post('/payment', (req, res) => {
   const body = {
